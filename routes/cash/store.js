6d189b3dfd8aeecacd2cfd39ab5f3b411395570f
aphinet12T <aphinet.dam@onetwotrading.co.th>
2025-01-10 10:39:56 +0700

    Update store.js

--- routes/cash/store.js
+++ routes/cash/store.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getStore, addStore, editStore, checkInStore } = require('../../controllers/store/storeController')
+const { getStore, addStore, editStore, checkInStore, addFromERP } = require('../../controllers/store/storeController')
 
 const router = express.Router()
 
 router.get('/getStore', getStore)
 router.post('/addStore', addStore)
+router.post('/addFromERP', addFromERP)
 router.patch('/editStore/:storeId', editStore)
 router.post('/checkIn/:storeId', checkInStore)
 
